a45a15006a2f5f5c5c43361514faf72880dc98e9
Jared <jared_cambier@brown.edu>
2022-04-27 12:09:09 -0400

    moved over hw5 for guidance

---
+++ code/parameters.py
@@ -0,0 +1,4 @@
+num_epochs = 100
+
+learning_rate = 1e-4
+
---
+++ code/run.py
@@ -0,0 +1,146 @@
+import os
+import sys
+import argparse
+import re
+from datetime import datetime
+import tensorflow as tf
+
+import parameters as hp
+from model import YourModel, VGGModel
+from preprocess import Datasets
+from skimage.transform import resize
+from tensorboard_utils import \
+        ImageLabelingLogger, CustomModelSaver
+
+from skimage.io import imread
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+from matplotlib import pyplot as plt
+import numpy as np
+
+def parse_args():
+    """ Perform command-line argument parsing. """
+
+    parser = argparse.ArgumentParser(
+        description="Let's train some neural nets!",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--train',
+        default=True,
+        help='Flag for whether to train the network on our dataset.')
+    parser.add_argument(
+        '--test',
+        default='../weights/example.h5',
+        help='Flag for whether to test the network using a set of weights.')
+    parser.add_argument(
+        '--blackjack',
+        default=False,
+        help='Starts the program using the weights inside of ../weights/preferred.')
+
+    return parser.parse_args()
+
+def train(model, datasets, checkpoint_path, logs_path, init_epoch):
+    """ Training routine. """
+
+    # Keras callbacks for training
+    callback_list = [
+        tf.keras.callbacks.TensorBoard(
+            log_dir=logs_path,
+            update_freq='batch',
+            profile_batch=0),
+        ImageLabelingLogger(logs_path, datasets),
+        CustomModelSaver(checkpoint_path, ARGS.task, hp.max_num_weights)
+    ]
+
+    # Begin training
+    model.fit(
+        x=datasets.train_data,
+        validation_data=datasets.test_data,
+        epochs=hp.num_epochs,
+        batch_size=None,
+        callbacks=callback_list,
+        initial_epoch=init_epoch,
+    )
+
+def test(model, test_data):
+    """ Testing routine. """
+
+    # Run model on test set
+    model.evaluate(
+        x=test_data,
+        verbose=1,
+    )
+
+def main():
+    """ Main function. """
+
+    time_now = datetime.now()
+    timestamp = time_now.strftime("%m%d%y-%H%M%S")
+    init_epoch = 0
+
+    # If paths provided by program arguments are accurate, then this will
+    # ensure they are used. If not, these directories/files will be
+    # set relative to the directory of run.py
+    if os.path.exists(ARGS.data):
+        ARGS.data = os.path.abspath(ARGS.data)
+    if os.path.exists(ARGS.load_vgg):
+        ARGS.load_vgg = os.path.abspath(ARGS.load_vgg)
+
+    # Run script from location of run.py
+    os.chdir(sys.path[0])
+
+    datasets = Datasets(ARGS.data, ARGS.task)
+
+    if ARGS.task == '1':
+        model = YourModel()
+        model(tf.keras.Input(shape=(hp.img_size, hp.img_size, 3)))
+        checkpoint_path = "checkpoints" + os.sep + \
+            "your_model" + os.sep + timestamp + os.sep
+        logs_path = "logs" + os.sep + "your_model" + \
+            os.sep + timestamp + os.sep
+
+        # Print summary of model
+        model.summary()
+    else:
+        model = VGGModel()
+        checkpoint_path = "checkpoints" + os.sep + \
+            "vgg_model" + os.sep + timestamp + os.sep
+        logs_path = "logs" + os.sep + "vgg_model" + \
+            os.sep + timestamp + os.sep
+        model(tf.keras.Input(shape=(224, 224, 3)))
+
+        # Print summaries for both parts of the model
+        model.vgg16.summary()
+        model.head.summary()
+
+        # Load base of VGG model
+        model.vgg16.load_weights(ARGS.load_vgg, by_name=True)
+
+    # Load checkpoints
+    if ARGS.load_checkpoint is not None:
+        if ARGS.task == '1':
+            model.load_weights(ARGS.load_checkpoint, by_name=False)
+        else:
+            model.head.load_weights(ARGS.load_checkpoint, by_name=False)
+
+    # Make checkpoint directory if needed
+    if not ARGS.evaluate and not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+
+    # Compile model graph
+    model.compile(
+        optimizer=model.optimizer,
+        loss=model.loss_fn,
+        metrics=["sparse_categorical_accuracy"])
+
+    if ARGS.evaluate:
+        test(model, datasets.test_data)
+
+        # TODO: change the image path to be the image of your choice by changing
+        # the lime-image flag when calling run.py to investigate
+        # i.e. python run.py --evaluate --lime-image test/Bedroom/image_003.jpg
+        path = ARGS.data + os.sep + ARGS.lime_image
+    else:
+        train(model, datasets, checkpoint_path, logs_path, init_epoch)
+
+ARGS = parse_args()
