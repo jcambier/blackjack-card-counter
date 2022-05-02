51df44d09efdda791e285efdc0874b2be53aba42
Gordon Jacobs <34612258+gjacobs314@users.noreply.github.com>
2022-05-01 22:31:30 -0400

    Better setup

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
-import multiprocessing
-import detect
-import subprocess
-import os
 import sys
 from subprocess import Popen, PIPE, STDOUT
+import pyfiglet
+
+detecting = False
+detect = ""
+
+def get_best_play(cards_detected):
+    return "GOOOO"
+
+def detect_cards():
+    weights = "best_weights.pt"
+    conf_treshold = 0.75
+    global detect
+    detect = Popen([sys.executable, '-u', "detect.py", "--source", "0", "--weights", weights, "--conf-thres", str(conf_treshold), "--nosave"], stdout=PIPE, stderr=STDOUT)
 
 def main():
-    weights = "best_weights.pt"
-    img_sz = (640, 640)
-    start = str(img_sz[1]) + " "
-    end = " Done."
-    detect = Popen([sys.executable, '-u', "detect.py", "--source", "0", "--weights", weights, "--nosave"], stdout=PIPE, stderr=STDOUT)
-    with detect.stdout:
+    pyfiglet.print_figlet("Blackjack Card Counter")
+    detect_cards()
+    while True:
+        input("Press enter to capture cards")
+        last_cards = ""
         for line in iter(detect.stdout.readline, b''):
             line = line.decode()
             if line[0] == "0":
-                cards = line[line.find(start)+len(start):line.rfind(end)]
-                if cards:
-                    print(cards)
-            else:
-                print(line.strip())
-    detect.wait()
+                cards_detected = line[line.find("640 ")+len("640 "):line.rfind(" Done.")]
+                if cards_detected:
+                    if last_cards != cards_detected:
+                        print("Detected cards: " + cards_detected)
+                        play = get_best_play(cards_detected)
+                        print("Based off of the cards present, you should %s\n" % play)
+                    last_cards = cards_detected
 
 if __name__ == "__main__":
     main()
