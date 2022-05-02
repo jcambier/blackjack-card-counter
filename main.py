f54c5e395c0fc261f09263641b6fbdedc5efebbb
Gordon Jacobs <34612258+gjacobs314@users.noreply.github.com>
2022-05-01 22:40:53 -0400

    Changed printing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from subprocess import Popen, PIPE, STDOUT
+import time
 import pyfiglet
 
 detecting = False
@@ -13,12 +14,15 @@ def detect_cards():
     conf_treshold = 0.75
     global detect
     detect = Popen([sys.executable, '-u', "detect.py", "--source", "0", "--weights", weights, "--conf-thres", str(conf_treshold), "--nosave"], stdout=PIPE, stderr=STDOUT)
+    time.sleep(5)
+    print("YOLO v5 detector started!\n")
 
 def main():
     pyfiglet.print_figlet("Blackjack Card Counter")
+    print("Starting YOLO v5 detector...")
     detect_cards()
     while True:
-        input("Press enter to capture cards")
+        input("Press enter to detect cards")
         last_cards = ""
         for line in iter(detect.stdout.readline, b''):
             line = line.decode()
@@ -30,6 +34,9 @@ def main():
                         play = get_best_play(cards_detected)
                         print("Based off of the cards present, you should %s\n" % play)
                     last_cards = cards_detected
+                else:
+                    print("No cards detected, please try again.\n")
+                    break
 
 if __name__ == "__main__":
     main()
