8d9a45229c1e366c32cb0fcf6789655783a517fd
Gordon Jacobs <34612258+gjacobs314@users.noreply.github.com>
2022-05-03 18:11:51 -0400

    Cleaned everything up a bit

--- main.py
+++ main.py
@@ -19,14 +19,26 @@ full_deck = {
     "K": {"c": 1, "d": 1, "h": 1, "s": 1},
     "A": {"c": 1, "d": 1, "h": 1, "s": 1}
 }
-cur_deck = full_deck.copy()
+
+def get_best_play(count):
+    running_count = 0
+    for i in range(13):
+        if 0 <= i <= 4:
+            running_count += 4 - count[i]
+        elif 8 <= i <= 12:
+            running_count -= 4 - count[i]
+    if running_count <= 0:
+        decision = "so you should bet low"
+    else: 
+        decision = "so you should bet high" 
+    print("the count is", running_count, decision)
 
 def count_cards():
     cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     count = [ 4,   4,   4,   4,   4,   4,   4,   4,    4,   4,   4,   4,   4 ]
     suits = ["c", "d", "h", "s"]
     for i, card in enumerate(cards):
-        suits = cur_deck.get(card)
+        suits = full_deck.get(card)
         for suit in suits:
             cnt = suits.get(suit)
             if cnt == 0:
@@ -47,44 +59,18 @@ def count_cards():
         "K": count[11],
         "A": count[12]
     }
-
-    running_count = 0
-    for i in range(13):
-        if 0 <= i <= 4:
-            running_count += 4 - count[i]
-        elif 8 <= i <= 12:
-            running_count -= 4 - count[i]
-    
-    
-    if running_count <= 0:
-        decision = "so you should bet low"
-    else: 
-        decision = "so you should bet high" 
-    
-
-    print("the count is", running_count, decision)
-    print(numbers_remaining)
-    return sum(count), numbers_remaining
-
-def get_best_play(cards_detected):
-    # here is where we tell the player what to do based on the cards left/value of cards left
-    total_cards, numbers_remaining = count_cards()
-    return "<do this>"
+    get_best_play(count)
+    print("remainingcards:", numbers_remaining, "\n")
 
 def process_cards(cards_detected):
     cards = cards_detected.split(", ")
     for card in cards:
-        print("card:", card)
         number = card[2]
-        print("number:", number)
         suit = card[3]
-        print("suit:", suit)
-
         if number == "1":
             number = "10"
             suit = card[4]
-        
-        cur_deck.get(str(number)).update({str(suit): 0})
+        full_deck.get(str(number)).update({str(suit): 0})
 
 def detect_cards():
     weights = "best_weights.pt"
@@ -109,9 +95,8 @@ def main():
                 if cards_detected:
                     cards_detected = cards_detected[:-1]
                     print("Detected cards: " + cards_detected)
-                    cards_detected = process_cards(cards_detected)
-                    play = get_best_play(cards_detected)
-                    print("Based off of the cards present, you should %s\n" % play)
+                    process_cards(cards_detected)
+                    count_cards()
 
 if __name__ == "__main__":
     main()
